f4b053c5503e12a7843553847e052646be78c84b
Rafhael <rafhael.dev@gmail.com>
2023-01-09 23:16:12 -0300

    adding an api description using openapi/swagger

--- api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
@@ -1,8 +1,13 @@
 package se.magnus.api.composite.product;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Api("REST API for composite product information.")
 public interface ProductCompositeService {
 
     /**
@@ -11,6 +16,14 @@ public interface ProductCompositeService {
      * @param productId
      * @return the composite product info, if found, else null
      */
+    @ApiOperation(
+            value = "${api.product-composite.get-composite-product.description}",
+            notes = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request, invalid format of the request. See response message for more information."),
+            @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
+            @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information.")
+    })
     @GetMapping(
         value    = "/product-composite/{productId}",
         produces = "application/json")
